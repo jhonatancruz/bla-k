0b34a12347b9f713bd8c97b1c30f5a79bbea2f1c
Jhonatan Cruz <jcruz3@drew.edu>
2016-09-19 20:42:00 -0400

    This is the first commit of the polygons program.

---
+++ Drawing_polygons/src/polygons.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.util.Scanner;
+import java.applet.*;
+
+public class polygons extends Applet {
+	int[] squarex = { 50, 50, 150, 150 };
+	int[] squarey = { 150, 50, 50, 150 };
+	int[] trix = { 65, 115, 165 };
+	int[] triy = { 150, 100, 150 };
+	int[] squarehx = { 25, 25, 150, 150 };
+	int[] squarehy = { 200, 100, 100, 200 };
+	int[] trihx = { 25, 88, 150 };
+	int[] trihy = { 100, 50, 100 };
+
+	public void init() {
+
+	}
+
+	public void paint(Graphics g) {
+		Scanner input = new Scanner(System.in);
+		String userinput = input.nextLine();
+
+		if (userinput.equals("house")) {
+			g.drawPolygon(squarehx, squarehy, 4);
+			g.drawPolygon(trihx, trihy, 3);
+		} else if (userinput.equals("triangle")) {
+			g.fillPolygon(trix, triy, 3);
+		} else if (userinput.equals("square")) {
+			g.fillPolygon(squarex, squarey, 4);
+		} else {
+			System.out.println("no input");
+		}
+
+	}
+
+}
+
+
+
