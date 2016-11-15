ab1d8bedda3b29171ed622fcaaf9e6c2cba0935d
Jhonatan Cruz <jcruz3@drew.edu>
2016-11-15 14:54:08 -0500

    Added a comment on temple of hercules to test

--- Drawing_polygons/src/blkGUIone/templeofherculer.java
+++ Drawing_polygons/src/blkGUIone/templeofherculer.java
@@ -18,61 +18,41 @@ class templeofherculer extends JPanel {
 		g.drawRect(15, 50, 100, 200);
 
 		// bottom four
-
 		g.drawArc(25, 230, 10, 10, 0, 360);
-
 		g.drawArc(50, 230, 10, 10, 0, 360);
-
 		g.drawArc(75, 230, 10, 10, 0, 360);
-
 		g.drawArc(100, 230, 10, 10, 0, 360);
-
 		g.fillArc(25, 230, 10, 10, 0, 360);// filling
-
 		g.fillArc(50, 230, 10, 10, 0, 360);
-
 		g.fillArc(75, 230, 10, 10, 0, 360);
-
 		g.fillArc(100, 230, 10, 10, 0, 360);
-
+		
 		// side two right
-
 		g.drawArc(100, 180, 10, 10, 0, 360);
-
 		g.fillArc(100, 180, 10, 10, 0, 360);
-
 		g.fillArc(100, 205, 10, 10, 0, 360);
-
 		g.drawArc(100, 205, 10, 10, 0, 360);
 
 		// side two left
-
 		g.drawArc(25, 180, 10, 10, 0, 360);
-
 		g.fillArc(25, 180, 10, 10, 0, 360); // filling
-
 		g.fillArc(25, 205, 10, 10, 0, 360);
-
 		g.drawArc(25, 205, 10, 10, 0, 360);
 
 		// across bar
-
 		g.fillRect(20, 60, 90, 8);
 
 		// left side bar
-
 		g.fillRect(20, 60, 10, 100);
 
 		// right side bar
-
 		g.fillRect(100, 60, 10, 100);
 
 		// across bar bottom right
-
 		g.fillRect(80, 155, 30, 8);
 
 		// across bar bottom left
-
 		g.fillRect(20, 155, 30, 8);
+		//this is a comment
 	}
 }
