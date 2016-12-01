32f73edf07bd8e2146c7604ab2a4dcac618f851a
Mike Brandao <Mbrandao@drew.edu>
2016-12-01 15:26:46 -0500

    Added Description

--- Drawing_polygons/src/blkGUIone/TEMPLE_TYPE.java
+++ Drawing_polygons/src/blkGUIone/TEMPLE_TYPE.java
@@ -3,7 +3,8 @@ package blkGUIone;
 public enum TEMPLE_TYPE {
 	PLEASE_SELECT_ONE_TEMPLE("",""),
 	ASIA_MINOR("The Temple at Teos in Asia Minor","Teos was one of the twelve cities."),
-	HERCULER("temple of herculer","Description");
+	HERCULER("temple of herculer","The Temple of Hercules Victor or Hercules Olivarius is a Roman temple in Piazza"
+			+ "\n Bocca della Verità, in the area of the Forum Boarium close to the Tiber in Rome.");
 	
 	String name="";
 	private String info="";
